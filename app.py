bf5a1d208cbf0c9a88deee6c10b2981a7490a6de
Aditi Dam <dama2468@gmail.com>
2022-01-08 22:01:00 -0500

    Added connection to database

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask,request, render_template
+from flask import Flask,request, render_template,g,Response, url_for,redirect
 
 #Flask app variable
 app = Flask(__name__)
+DATABASEURI = "postgresql://aditi:350Cochran@localHost:5432/securedhealth"
+
+engine = create_engine(DATABASEURI)
+
+@app.before_request
+def before_request():
+  try:
+    g.conn = engine.connect()
+  except:
+    print("Cannot connect to database")
+    import traceback; traceback.print_exc()
+    g.conn = None
+
+@app.teardown_request
+def teardown_request(exception):
+  
+  try:
+    g.conn.close()
+  except Exception as e:
+    pass
+    
 
 #static route
 @app.route("/")
@@ -12,8 +33,12 @@ def home():
 def homeBack():
     return render_template("index.html")
 
-@app.route("/schedule")
+@app.route('/schedule', methods = ["GET", "POST"])
 def schedule():
+        username = request.args.get('user')
+        if request.method == "POST":
+            speciality = request.form['speciality']
+            return redirect(url_for('availabledoctors',user = username,speciality = speciality))
         return render_template("schedule.html")
 
 @app.route("/confirmschedule")
@@ -23,12 +48,14 @@ def confirmschedule():
 @app.route("/confirmation")
 def confirmation():
         return render_template("confirmation.html")
+        
+@app.route("/dashboard")
+def confirmation():
+        return render_template("dashboard.html")
 
 @app.route("/availabledoctors")
 def availabledoctors():
         return render_template("availabledoctors.html")
-
-
 
 @app.route("/createaccount",methods = ["GET", "POST"])
 def gfg():
@@ -53,16 +80,26 @@ def gfg():
 		height = request.form.get("height")
 		zip = request.form.get("zipcode")
 		state = request.form.get("state")
+  
+        try:
+            g.conn.execute('INSERT INTO Users(name, breed, birthday, sex, profile_picture, bio, username, size, build) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)', name, breed, birthday, sex, profile_picture, bio, username, size, build)
+            
+        except Exception:
+            error = 'Invalid entry'
+            
+        if error is None:
+            return redirect(url_for('dashboard',user = username))
 
-		return "<h1>Patient Information</h1> <br> <h5>Full Name:</h5> " +  first_name + " " + last_name  + "<br><h5>Gender: </h5>" + gender + "<br> <h5>Age:</h5> " + age + "<br> <h5>Weight: </h5>" + weight + "<br> <h5>height</h5>" + height + "<br> <h5>Primary Contact Number:</h5> " + mainNum + "<br> <h5>Alternative Phone: </h5>" + altNum + "<br><h4>Emergency Contact Info:</h4> <br> <h5>Name</h5> <br>" + emergfirst + " " + emerglast + "<br><h5>Number:</h5>" + emergNumber
-	return render_template("createaccount.html")
+#		return "<h1>Patient Information</h1> <br> <h5>Full Name:</h5> " +  first_name + " " + last_name  + "<br><h5>Gender: </h5>" + gender + "<br> <h5>Age:</h5> " + age + "<br> <h5>Weight: </h5>" + weight + "<br> <h5>height</h5>" + height + "<br> <h5>Primary Contact Number:</h5> " + mainNum + "<br> <h5>Alternative Phone: </h5>" + altNum + "<br><h4>Emergency Contact Info:</h4> <br> <h5>Name</h5> <br>" + emergfirst + " " + emerglast + "<br><h5>Number:</h5>" + emergNumber
+	
+    return render_template("createaccount.html")
 
 @app.route("/myaccount", methods = ["GET", "POST"])
 def myaccount():
 	if request.method == "POST":
 		username = request.form.get("username")
 		password = request.form.get("password")
-		return "<h1>Patient Information</h1>"
+		return redirect(url_for('dashboard',user = username))
 	return render_template("myaccount.html")
 
 #start the server
