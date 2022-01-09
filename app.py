286320ceba6fc02434c07018ccc59725d9c10897
Aditi Dam <dama2468@gmail.com>
2022-01-08 23:03:28 -0500

    Fixed indents

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import os
+from sqlalchemy import *
+from sqlalchemy.pool import NullPool
+
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+
 from flask import Flask,request, render_template,g,Response, url_for,redirect
 
 #Flask app variable
@@ -41,15 +47,15 @@ def schedule():
     if len(speciality) != 0:
         try:
             cursor = g.conn.execute('SELECT D.firstName, D.lastName FROM Doctors D WHERE D.speciality = (%s)')
-    for result in cursor:
-        firstNames.append(result['firstName'])
-        lastNames.append(result['lastName'])
-    cursor.close()
+            for result in cursor:
+                firstNames.append(result['firstName'])
+                lastNames.append(result['lastName'])
+            cursor.close()
 
-    context_firstNames = dict(data_one = firstNames)
-    context_lastNames = dict(data_two = lastNames)
-    except Exception:
-        error = 'Search failed'
+            context_firstNames = dict(data_one = firstNames)
+            context_lastNames = dict(data_two = lastNames)
+        except Exception:
+            error = 'Search failed'
     return render_template("availabledoctors", error = error, user = username, **context_firstNames, **context_lastNames)
 
     if request.method == "POST":
@@ -86,29 +92,29 @@ def availabledoctors():
 
 @app.route("/createaccount",methods = ["GET", "POST"])
 def gfg():
-	if request.method == "POST":
+    if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
-		Firstname = request.form.get("fname")
-		Lastname = request.form.get("lname")
-		DOB = request.form.get("bday")
-		PhoneNumber = request.form.get("mainPhone")
-		AltPhone = request.form.get("altPhone")
-		Email = request.form.get("email")
-		Age = request.form.get("age")
-		Gender = request.form.get("gender")
-		EmergFirstName = request.form.get("emergFname")
-		EmergLastName = request.form.get("emergLname")
-		Relationship = request.form.get("relation")
-		EmergNumber = request.form.get("emergNumber")
-		PharmName = request.form.get("pharmName")
-		PharmNum = request.form.get("pharmNum")
-		DoctorName = request.form.get("primaryName")
-		DoctorNum = request.form.get("primaryNum")
+        Firstname = request.form.get("fname")
+        Lastname = request.form.get("lname")
+        DOB = request.form.get("bday")
+        PhoneNumber = request.form.get("mainPhone")
+        AltPhone = request.form.get("altPhone")
+        Email = request.form.get("email")
+        Age = request.form.get("age")
+        Gender = request.form.get("gender")
+        EmergFirstName = request.form.get("emergFname")
+        EmergLastName = request.form.get("emergLname")
+        Relationship = request.form.get("relation")
+        EmergNumber = request.form.get("emergNumber")
+        PharmName = request.form.get("pharmName")
+        PharmNum = request.form.get("pharmNum")
+        DoctorName = request.form.get("primaryName")
+        DoctorNum = request.form.get("primaryNum")
         Height = request.form.get("height")
-		Weight = request.form.get("weight")
-		zip = request.form.get("zipcode")
-		state = request.form.get("state")
+        Weight = request.form.get("weight")
+        zip = request.form.get("zipcode")
+        state = request.form.get("state")
 
         try:
             g.conn.execute('INSERT INTO Users(username,password,Firstname,Lastname,  DOB,PhoneNumber,AltPhone,Email,Age,Gender,EmergFirstName, EmergLastName, Relationship,EmergNumber,PharmName,PharmNum,DoctorName,DoctorNum,Height, Weight, streetAddress, zip, state, city) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s,%s,%s,%s,%s,%s,%s,%s,%d,%d,%s,%d,%s,%s)',username,password,Firstname,Lastname,  DOB,PhoneNumber,AltPhone,Email,Age,Gender,EmergFirstName, EmergLastName, Relationship,EmergNumber,PharmName,PharmNum,DoctorName,DoctorNum,Height, Weight, streetAddress, zip, state, city)
@@ -125,7 +131,7 @@ def gfg():
 def myaccount():
     if request.method == "POST":
         username = request.form['username']
-		password = request.form['password']
+        password = request.form['password']
         login = []
         try:
             cursor = g.conn.execute('SELECT username FROM Users WHERE username = (%s) AND password = (%s)', username, password)
